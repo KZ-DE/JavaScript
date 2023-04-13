77f07260d6293a2a4f07a25388c631258955db13
KZ-DE <nebulacom005@gmail.com>
2023-04-13 18:38:23 +0700

    operator dan var

---
+++ Dasar Js/operator/operator perbandingan/index.js
@@ -0,0 +1,32 @@
+let a = 10;
+let b = 5;
+let c = "10";
+let hasil;
+
+hasil = a > b;
+console.log(hasil);
+
+hasil = a < b;
+console.log(hasil);
+
+hasil = a >= b;
+console.log(hasil);
+
+hasil = a <= b;
+console.log(hasil);
+
+// satu tipe data
+hasil = a == b;
+console.log(hasil);
+
+// beda tipe data
+hasil = a === c;
+console.log(hasil);
+
+// satu tipe data
+hasil = a != b;
+console.log(hasil);
+
+// beda tipe data
+hasil = a !== c;
+console.log(hasil);
